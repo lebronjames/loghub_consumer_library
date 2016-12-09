4b2d2256c2bfabe5dccb3f40a6240f4cf0622481
yonghualuo <yonghua.lyh@120yibao.com>
2016-12-09 13:14:55 +0800

    modify defaultThreadFactory

---
+++ src/main/java/com/aliyun/openservices/loghub/client/LogThreadFactory.java
@@ -0,0 +1,29 @@
+package com.aliyun.openservices.loghub.client;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by yonghualuo on 2016/12/9.
+ */
+public class LogThreadFactory implements ThreadFactory {
+    private static final AtomicInteger poolNumber = new AtomicInteger(1);
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+
+    LogThreadFactory() {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        namePrefix = "aliyun-log-pool-" + poolNumber.getAndIncrement() + "-thread-";
+    }
+
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+        if (!t.isDaemon())
+            t.setDaemon(true);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+}
